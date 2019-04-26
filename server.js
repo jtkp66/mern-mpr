cf9f6bcca50d9a3dad1b7efae403b2af2dea51ba
Jamie Pham <jtkp66@gmail.com>
2019-04-26 16:50:47 -0700

    Connected server to MongoDB with monngoose.

--- server.js
+++ server.js
@@ -4,6 +4,13 @@ const passport = require("passport");
 
 const app = express();
 
+const db = require("./config/keys").mongoURI;
+
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("MongoDB Connected..."))
+  .catch(err => console.log(err));
+
 app.get("/", (req, res) => res.send("Hello"));
 
 const port = process.env.PORT || 5000;
