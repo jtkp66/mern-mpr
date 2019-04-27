91643932b5ae67b9b458f889aafe2c3957a35d15
Jamie Pham <jtkp66@gmail.com>
2019-04-26 23:48:28 -0700

    Added User model to users route

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
+// Load User model
+const User = require("../../models/User");
+
 // @route   GET api/users/test
 // @desc    Tests users route
 // @access  Public
