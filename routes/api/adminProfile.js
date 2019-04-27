008ed094ff02f49896e631f12f59ddfdb854be35
Jamie Pham <jtkp66@gmail.com>
2019-04-26 23:39:40 -0700

    Created User model for MongoDB with mongoose schema

--- routes/api/adminProfile.js
+++ routes/api/adminProfile.js
@@ -4,6 +4,6 @@ const router = express.Router();
 // @route   GET api/users/test
 // @desc    Tests users route
 // @access  Public
-router.get("/test", (req, res) => res.json({ msg: "Profile Works" }));
+router.get("/test", (req, res) => res.json({ msg: "Admin Profile Works" }));
 
 module.exports = router;
