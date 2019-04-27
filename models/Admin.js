a0dfece2866ee6cf000a6f616a6dde87f8e68579
Jamie Pham <jtkp66@gmail.com>
2019-04-26 23:41:05 -0700

    Created User admin for MongoDB with mongoose schema

---
+++ models/Admin.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// Create Schema
+const AdminSchema = new Schema({
+  firstname: {
+    type: String,
+    required: true
+  },
+  lastname: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = User = mongoose.model("admin", AdminSchema);
